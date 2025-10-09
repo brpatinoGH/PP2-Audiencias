20914a2fb24d77a06d601817eb3fe01e9681a05b
Brunella Patiño <brunepatino@gmail.com>
2025-10-08 22:19:51 -0300

    feat(error)Add custom BusinessException and NotFoundException

---
+++ backend/audiencia-service/src/main/java/com/justicia/audiencia_service/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.justicia.audiencia_service.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
---
+++ backend/audiencia-service/src/main/java/com/justicia/audiencia_service/exception/NotFoundException.java
@@ -0,0 +1,5 @@
+package com.justicia.audiencia_service.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String msg) { super(msg); }
+}
